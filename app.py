0de69f4eed15f701647495723b6d600038da61e4
Parth Patel <maganp00gmail.com>
2022-04-30 18:07:35 -0400

    followed directions with commenting a line

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
     """ uncomment at the first time running the app """
-    db_drop_and_create_all()
+    # db_drop_and_create_all()
 
     @app.route('/', methods=['GET'])
     def home():
