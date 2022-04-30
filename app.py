3ea5fc815852f45e08378a577f8158f141e518a2
Parth Patel <maganp00gmail.com>
2022-04-30 16:46:36 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_cors import CORS
+from models import setup_db, Url, db_drop_and_create_all
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+    """ uncomment at the first time running the app """
+    db_drop_and_create_all()
+
+    @app.route('/', methods=['GET'])
+    def home():
+        return jsonify({'message': 'Hello,hello, World!'})
+
+    @app.route("/movies")
+    def get_movies():
+        try:
+            urls = Url.query.order_by(Url.id).all()
+            url=[]
+            url=[mov.url for mov in urls]
+            return jsonify(
+                {
+                    "success": True,
+                    "url": url
+                }
+            ), 200
+        except:
+            abort(500)
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "server error"
+        }), 500
+    return app
+
+app = create_app()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port,debug=True)
---
+++ setup.sh
@@ -0,0 +1,1 @@
+export DATABASE_URL = 'postgres://nnpcdjftqzikzp:0c9a3bf3b17c2913a8959b69f1be4aea9c8dbc7c3b0dd72ab79cee2b9c88948d@ec2-52-5-110-35.compute-1.amazonaws.com:5432/d3ps0m1h3ghotm'
